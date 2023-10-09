cfd4d6db3131cde3ab83f1ae264fb19cd84a1745
Ahmed Fouad <68746856+ahmedfouad7@users.noreply.github.com>
2023-10-09 19:36:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 st.set_page_config( initial_sidebar_state="collapsed",
-    page_title="Delve Health Wearabel Measurements",
+    page_title="Adva",
     page_icon="👋",
     layout='wide' 
     )
@@ -26,20 +26,8 @@ st.write("# Welcome to Streamlit! 👋")
 # st.sidebar.success("Select a demo above.")
 st.markdown(
     f"""
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+  ## Medical Procedures on Flexible Installments
 
-    ## The Parameters are {parameters}
+**Pay on flexible installment plans for any surgical operation or medical procedure with ADVA. Choose from a wide network of doctors, clinics & hospitals to book your medical procedure on flexible installments up to 24 months and with the lowest interest price. Pay your operation expenses for up to 60 months.**
 """
 )
